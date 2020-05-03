d39c996446501ceb123d33d7b19fbcdba65bacf8
Vincent Serpoul <vincent@serpoul.com>
2020-05-04 01:44:39 +0800

    debug

--- applications/build-n-push-all.sh
+++ applications/build-n-push-all.sh
@@ -17,6 +17,9 @@ if [[ ! -d "$DIR_BUILD" ]]; then DIR_BUILD="$PWD"; fi
 readonly BASE_DOCKER_REPO=${1:-vincentserpoul}
 readonly RAW_HTTP_GET_APPS=("$DIR_BUILD"/raw-http-get/*)
 
+echo "$BASE_DOCKER_REPO"
+echo "${RAW_HTTP_GET_APPS[@]##*/}"
+
 for app in "${RAW_HTTP_GET_APPS[@]##*/}"; do
     docker build "$DIR_BUILD"/raw-http-get/"$app" \
         --rm -t "$BASE_DOCKER_REPO"/kubench-"$app" \
