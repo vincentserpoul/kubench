86c7a4d753e39a08a7d499300963cbf58aff96d9
Vincent Serpoul <vincent@serpoul.com>
2020-10-12 23:58:57 +0800

    rewire the routes

--- applications/rest-petstore/ruby-on-rails/config/routes.rb
+++ applications/rest-petstore/ruby-on-rails/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    resources :pets
+    resources :pets, path: 'pet'
     resources :categories
-  end
 end
